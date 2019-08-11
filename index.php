5f84a0a92a85bf7ac2556f996d0e46da880e92bb
Nathanael Lima <nathanaellimacpc@gmail.com>
2019-08-11 19:53:06 -0300

    Criação dos controllers

--- index.php
+++ index.php
@@ -2,7 +2,15 @@
 // Include config and Bootstrap
 require 'config.php';
 require 'classes/bootstrap.php';
+require 'classes/Controller.php';
+
+require 'controllers/Home.php';
+require 'controllers/Users.php';
+require 'controllers/Shares.php';
 
 $bootstrap = new Bootstrap($_GET);
 
 $controller = $bootstrap->createController();
+if ($controller) {
+    $controller->executeAction();
+}
