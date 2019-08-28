d32c9c82cec37683c6b14218de94fbb75a3a0a22
Nathanael Lima <nathanaellimacpc@gmail.com>
2019-08-27 22:19:17 -0300

    alteração da url e base folder do projeto

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 // Definir parâmetros do BD
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', '123456');
 define('DB_NAME', 'shareboard');
 
 // Definir urls
-define('ROOT_PATH', '/examples/shareboard');
-define('ROOT_URL', 'http://localhost/examples/shareboard');
+define('ROOT_PATH', '/shareboard');
+define('ROOT_URL', 'http://localhost/shareboard');
